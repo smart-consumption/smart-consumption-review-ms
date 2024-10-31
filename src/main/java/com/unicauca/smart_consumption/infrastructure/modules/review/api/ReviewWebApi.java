e7c0a505ca66eb9b13905c1cfac5706fda7aaa2f
JulianRuano <julianrmajin@gmail.com>
2024-10-30 21:24:02 -0500

    Add sql init

--- src/main/java/com/unicauca/smart_consumption/infrastructure/modules/review/api/ReviewWebApi.java
+++ src/main/java/com/unicauca/smart_consumption/infrastructure/modules/review/api/ReviewWebApi.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
